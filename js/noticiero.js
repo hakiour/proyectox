77b213c985867e66cd778a8655d4535d53d2ab5c
Hamza Akiour <contacto@hamza.es>
2018-05-15 09:56:57 +0200

    Añadido link, descripción y imagen personalizable según información que hay en XML

--- js/noticiero.js
+++ js/noticiero.js
@@ -12,15 +12,18 @@ function loadXMLDoc() {
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  var table;
+  var contenido = "";
   var x = xmlDoc.getElementsByTagName("NOTICIA");
   for (i = 0; i <x.length; i++) {
-    table += "<div class='jumbotron'><div id='header'><h1 class='header text-center text-uppercase' id='target1'><u>" +
+    contenido += "<div class='jumbotron'><div id='header'><h1 class='header text-center text-uppercase' id='target1'><u>" +
     x[i].getElementsByTagName("TITULO")[0].childNodes[0].nodeValue +
     "</u></h1><h2 class='title text-center text-uppercase' id='target2'><em>" +
-    x[i].getElementsByTagName("DESCRIPCION")[0].childNodes[0].nodeValue +
-    "</em></h2><div class='thumbnail'>          <img class='img-responsive center-block zoom' src='../images/ralarsaPortada.png' height='100px' id='target3' alt 'Imagen portada'>          <div class='caption text-center' id='target4'>            Imagen extraída de la revista Ralarsa News - Edición 9          </div>        </div></div> <div id='context' class='text-justify'>        <h3 id='target5'>Introducción</h3> Es evidente que el sector del automóvil se encuentra inmerso en una profunda transformación, liderando lo que se ha denominado “Industria 4.0”.        <br>        <br>        <citer class='text-left' id='target7'>          Auto Cristal Ralarsa 2017 - 2018        </citer>        <h3 class='text-center' id='target8'><a href='noticias/noticia-1.html'> Seguir leyendo </a></h3> </div>     </div>";
+    x[i].getElementsByTagName("SUBTITULO")[0].childNodes[0].nodeValue +
+    "</em></h2><div class='thumbnail'><center><img class='img-responsive center-block zoom' src='../images/noticias/" +
+    x[i].getElementsByTagName("IMAGEN")[0].childNodes[0].nodeValue + "' heigt='100px' id='target3' alt 'Imagen portada'></center>          <div class='caption text-center' id='target4'>            Todos los derechos reservados          </div>        </div></div> <div id='context' class='text-justify'>        <h3 id='target5'>Introducción</h3> "+
+    x[i].getElementsByTagName("DESCRIPCION")[0].childNodes[0].nodeValue + "         <br>        <br>        <citer class='text-left' id='target7'>          ProyectoX 2017 - 2018        </citer>        <h3 class='text-center' id='target8'><a href='noticias/"+
+    x[i].getElementsByTagName("LINK")[0].childNodes[0].nodeValue + "'> Seguir leyendo </a></h3> </div>     </div>";
 
   }
-  document.getElementById("ultimasNoticias").innerHTML = table;
+  document.getElementById("ultimasNoticias").innerHTML = contenido;
 }

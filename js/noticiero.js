50b48d3c9de78d9d63eb2a53e6c9a17fd6783dc4
Hamza Akiour <contacto@hamza.es>
2018-05-15 10:13:46 +0200

    Cambiadas etiquetas de mayusculas a minusculas en noticiero.js (ya que las etiquetas de noticias.xml se han cambiado para la validación del DTD)

--- js/noticiero.js
+++ js/noticiero.js
@@ -3,26 +3,26 @@ function loadXMLDoc() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
+      cargarContenido(this);
     }
   };
   xmlhttp.open("GET", "../xml/noticias.xml", true);
   xmlhttp.send();
 }
-function myFunction(xml) {
+function cargarContenido(xml) {
   var i;
   var xmlDoc = xml.responseXML;
   var contenido = "";
-  var x = xmlDoc.getElementsByTagName("NOTICIA");
+  var x = xmlDoc.getElementsByTagName("noticia");
   for (i = 0; i <x.length; i++) {
     contenido += "<div class='jumbotron'><div id='header'><h1 class='header text-center text-uppercase' id='target1'><u>" +
-    x[i].getElementsByTagName("TITULO")[0].childNodes[0].nodeValue +
+    x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +
     "</u></h1><h2 class='title text-center text-uppercase' id='target2'><em>" +
-    x[i].getElementsByTagName("SUBTITULO")[0].childNodes[0].nodeValue +
+    x[i].getElementsByTagName("subtitulo")[0].childNodes[0].nodeValue +
     "</em></h2><div class='thumbnail'><center><img class='img-responsive center-block zoom' src='../images/noticias/" +
-    x[i].getElementsByTagName("IMAGEN")[0].childNodes[0].nodeValue + "' heigt='100px' id='target3' alt 'Imagen portada'></center>          <div class='caption text-center' id='target4'>            Todos los derechos reservados          </div>        </div></div> <div id='context' class='text-justify'>        <h3 id='target5'>Introducción</h3> "+
-    x[i].getElementsByTagName("DESCRIPCION")[0].childNodes[0].nodeValue + "         <br>        <br>        <citer class='text-left' id='target7'>          ProyectoX 2017 - 2018        </citer>        <h3 class='text-center' id='target8'><a href='noticias/"+
-    x[i].getElementsByTagName("LINK")[0].childNodes[0].nodeValue + "'> Seguir leyendo </a></h3> </div>     </div>";
+    x[i].getElementsByTagName("imagen")[0].childNodes[0].nodeValue + "' heigt='100px' id='target3' alt 'Imagen portada'></center>          <div class='caption text-center' id='target4'>            Todos los derechos reservados          </div>        </div></div> <div id='context' class='text-justify'>        <h3 id='target5'>Introducción</h3> "+
+    x[i].getElementsByTagName("descripcion")[0].childNodes[0].nodeValue + "         <br>        <br>        <citer class='text-left' id='target7'>          ProyectoX 2017 - 2018        </citer>        <h3 class='text-center' id='target8'><a href='noticias/"+
+    x[i].getElementsByTagName("link")[0].childNodes[0].nodeValue + "'> Seguir leyendo </a></h3> </div>     </div>";
 
   }
   document.getElementById("ultimasNoticias").innerHTML = contenido;

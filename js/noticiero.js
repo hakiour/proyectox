dd65297f5b25e24752bdb80c9f78aa0836ce0b31
Hamza Akiour <contacto@hamza.es>
2018-05-15 09:34:52 +0200

    Añadida función que carga las noticias a partir del XML, puesto index.html en la raiz del programa, etc.

--- js/noticiero.js
+++ js/noticiero.js
@@ -12,14 +12,15 @@ function loadXMLDoc() {
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  var table="<tr><th>Artist</th><th>Title</th></tr>";
-  var x = xmlDoc.getElementsByTagName("CD");
+  var table;
+  var x = xmlDoc.getElementsByTagName("NOTICIA");
   for (i = 0; i <x.length; i++) {
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-    "</td></tr>";
+    table += "<div class='jumbotron'><div id='header'><h1 class='header text-center text-uppercase' id='target1'><u>" +
+    x[i].getElementsByTagName("TITULO")[0].childNodes[0].nodeValue +
+    "</u></h1><h2 class='title text-center text-uppercase' id='target2'><em>" +
+    x[i].getElementsByTagName("DESCRIPCION")[0].childNodes[0].nodeValue +
+    "</em></h2><div class='thumbnail'>          <img class='img-responsive center-block zoom' src='../images/ralarsaPortada.png' height='100px' id='target3' alt 'Imagen portada'>          <div class='caption text-center' id='target4'>            Imagen extraída de la revista Ralarsa News - Edición 9          </div>        </div></div> <div id='context' class='text-justify'>        <h3 id='target5'>Introducción</h3> Es evidente que el sector del automóvil se encuentra inmerso en una profunda transformación, liderando lo que se ha denominado “Industria 4.0”.        <br>        <br>        <citer class='text-left' id='target7'>          Auto Cristal Ralarsa 2017 - 2018        </citer>        <h3 class='text-center' id='target8'><a href='noticias/noticia-1.html'> Seguir leyendo </a></h3> </div>     </div>";
+
   }
-  document.getElementById("demo").innerHTML = table;
+  document.getElementById("ultimasNoticias").innerHTML = table;
 }

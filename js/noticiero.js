31cbe372bf14f71cd80a87893c4b8a8cfd9d67f6
Hamza Akiour <akiourh@gmail.com>
2018-05-25 22:52:50 +0200

    Cambios realizados: -Añadido un apartado de 'Equipo' donde el usuario puede ver información y/o acceder a las redes sociales de los miembros del proyecto -Añadido el apartado 'Metodología de trabo' que describe como hemos trabajado en este proyecto -Quitadas las fuentes que no se estaban utilizando -Añadidos algunos comentarios para describir mejor según que apartados -Añadido un parallax -Varios cambios menores

--- js/noticiero.js
+++ js/noticiero.js
@@ -1,7 +1,10 @@
-window.onload = loadXMLDoc;
+window.onload = loadXMLDoc; 
 function loadXMLDoc() {
+  //Funcionq ue carga el fichero XML
   var xmlhttp = new XMLHttpRequest();
+  
   xmlhttp.onreadystatechange = function() {
+    //Una vez cargado, llamamos a la función que llena el noticiero con las noticias
     if (this.readyState == 4 && this.status == 200) {
       cargarContenido(this);
     }
@@ -10,19 +13,19 @@ function loadXMLDoc() {
   xmlhttp.send();
 }
 function cargarContenido(xml) {
-  var i;
+  //Insertamos las noticias dentro de la estructura HTML
   var xmlDoc = xml.responseXML;
   var contenido = "";
   var x = xmlDoc.getElementsByTagName("noticia");
-  for (i = 0; i <x.length; i++) {
+  for (var indice = 0; indice <x.length; indice++) {
     contenido += "<div class='jumbotron'><div id='header'><h1 class='header text-center text-uppercase' id='target1'><u>" +
-    x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +
+    x[indice].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +
     "</u></h1><h2 class='title text-center text-uppercase' id='target2'><em>" +
-    x[i].getElementsByTagName("subtitulo")[0].childNodes[0].nodeValue +
+    x[indice].getElementsByTagName("subtitulo")[0].childNodes[0].nodeValue +
     "</em></h2><div class='thumbnail'><center><img class='img-responsive center-block zoom' src='../images/noticias/" +
-    x[i].getElementsByTagName("imagen")[0].childNodes[0].nodeValue + "' heigt='100px' id='target3' alt 'Imagen portada'></center>          <div class='caption text-center' id='target4'>            Todos los derechos reservados          </div>        </div></div> <div id='context' class='text-justify'>        <h3 id='target5'>Introducción</h3> "+
-    x[i].getElementsByTagName("descripcion")[0].childNodes[0].nodeValue + "         <br>        <br>        <citer class='text-left' id='target7'>          HamRub 2017 - 2018        </citer>        <h3 class='text-center' id='target8'><a href='noticias/"+
-    x[i].getElementsByTagName("link")[0].childNodes[0].nodeValue + "'> Seguir leyendo </a></h3> </div>     </div>";
+    x[indice].getElementsByTagName("imagen")[0].childNodes[0].nodeValue + "' heigt='100px' id='target3' alt 'Imagen portada'></center>          <div class='caption text-center' id='target4'>            Todos los derechos reservados          </div>        </div></div> <div id='context' class='text-justify'>        <h3 id='target5'>Introducción</h3> "+
+    x[indice].getElementsByTagName("descripcion")[0].childNodes[0].nodeValue + "         <br>        <br>        <citer class='text-left' id='target7'>          HamRub 2017 - 2018        </citer>        <h3 class='text-center' id='target8'><a href='noticias/"+
+    x[indice].getElementsByTagName("link")[0].childNodes[0].nodeValue + "'> Seguir leyendo </a></h3> </div>     </div>";
 
   }
   document.getElementById("ultimasNoticias").innerHTML = contenido;
